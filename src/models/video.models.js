f8f92235477faf862ef4c6ab53f1327c5e3914aa
Roshan Vishwakarma <roshanvishwakarma089@gmail.com>
2025-08-17 16:47:49 +0530

    user video models update

---
+++ src/models/video.models.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+    {
+        videofile: {
+            type: String, // cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, // cloudinary url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number, // cloudinary duration
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: false
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
